3fbc50de99fc5c382b9cdabfa46c9e4a6ffe2d39
vribli <vribli@gmail.com>
2020-03-19 15:04:05 +0800

    added small comment

--- propertyinfo/views.py
+++ propertyinfo/views.py
@@ -7,10 +7,14 @@ def index(request):
         name = request.GET['name']
         postal = request.GET['postal']
 
+        // your code here
+
         context = {
             'name' : name,
             'postal' : postal
         }
+
+
         return render(request, "propertyinfo/index.html", context)
     except KeyError:
         return HttpResponse("Access through home, please")
