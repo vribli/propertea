fec8ed661c912ddcfc34bed25473f1343c80736a
vribli <47019043+vribli@users.noreply.github.com>
2020-03-20 01:11:29 +0800

    Allow pythonanywhere

--- propertea/settings.py
+++ propertea/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%b5g9g9l)k@_g)6lj#-19*eh9+-4ja4dy^!ceks+vxo=%m@tuf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vribli.pythonanywhere.com']
 
 
 # Application definition
