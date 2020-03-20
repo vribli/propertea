0c855929b90cfb98dfd854523109ac82b96f9c64
Johnson Foo <johnsonfoo18@gmail.com>
2020-03-20 22:57:17 +0800

    Added mrt volume with plotly

--- propertyinfo/views.py
+++ propertyinfo/views.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
 from django.shortcuts import render
-import requests 
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.offline import plot
 
 # Create your views here.
 def index(request):
@@ -8,18 +9,33 @@ def index(request):
         name = request.GET['name']
         postal = request.GET['postal']
         
-        r = requests.get("https://www.squarefoot.com.sg/trends-and-analysis/residential?p={name_formatted}".format(name_formatted = name.replace(" ", "-"))) 
+        #add in colab code here, uncomment plot afterwards
+        MRT_DataFrame = pd.DataFrame(pd.read_csv("C:/Users/johns/Desktop/MRT_Passenger_Volume.csv"))
+        Final_Data = MRT_DataFrame[MRT_DataFrame['PT_CODE']=='BP1'][MRT_DataFrame['DAY_TYPE']=='WEEKDAY'].sort_values('TIME_PER_HOUR')
+        time = [24 if x == 0 else x for x in Final_Data['TIME_PER_HOUR'].tolist()]
 
-        info_raw = BeautifulSoup(r.content).find('table',{"class":"minimalist", "width":"95%"}).text.replace("\n\n", "").replace("#", "").replace("*", "").split("\n")
-        info_heading = list(x for x in info_raw if info_raw.index(x) % 2 == 0)
-        info_content = list(x for x in info_raw if info_raw.index(x) % 2 == 1)
-        info_dict = dict(zip(info_heading, info_content))
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            x=time,
+            y=Final_Data['TOTAL_TAP_IN_VOLUME'].tolist(),
+            name='Total Tap In Volume',
+            marker_color='red'
+        ))
+        fig.add_trace(go.Bar(
+            x=time,
+            y=Final_Data['TOTAL_TAP_OUT_VOLUME'].tolist(),
+            name='Total Tap Out Volume',
+            marker_color='orange'
+        ))
 
-        print(info_dict)
+        # Here we modify the tickangle of the xaxis, resulting in rotated labels.
+        fig.update_layout(barmode='group', xaxis_tickangle=-45)
+        plot_div = plot(fig, output_type="div", include_plotlyjs=False)
+
         context = {
             'name' : name,
             'postal' : postal,
-            'info_dict' : info_dict
+            'plot' : plot_div
         }
 
         return render(request, "propertyinfo/index.html", context)
