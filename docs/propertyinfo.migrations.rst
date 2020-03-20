98e21261e7466b94207021dfc9151376612bb04d
Johnson Foo <johnsonfoo18@gmail.com>
2020-03-20 15:41:34 +0800

    Made first working logic for displaying table info from squarefoot

---
+++ docs/propertyinfo.migrations.rst
@@ -0,0 +1,10 @@
+propertyinfo.migrations package
+===============================
+
+Module contents
+---------------
+
+.. automodule:: propertyinfo.migrations
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- propertyinfo/views.py
+++ propertyinfo/views.py
@@ -1,16 +1,25 @@
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+import requests 
 
 # Create your views here.
 def index(request):
     try:
         name = request.GET['name']
         postal = request.GET['postal']
+        
+        r = requests.get("https://www.squarefoot.com.sg/trends-and-analysis/residential?p={name_formatted}".format(name_formatted = name.replace(" ", "-"))) 
 
-        # your code here
+        info_raw = BeautifulSoup(r.content).find('table',{"class":"minimalist", "width":"95%"}).text.replace("\n\n", "").replace("#", "").replace("*", "").split("\n")
+        info_heading = list(x for x in info_raw if info_raw.index(x) % 2 == 0)
+        info_content = list(x for x in info_raw if info_raw.index(x) % 2 == 1)
+        info_dict = dict(zip(info_heading, info_content))
 
+        print(info_dict)
         context = {
             'name' : name,
-            'postal' : postal
+            'postal' : postal,
+            'info_dict' : info_dict
         }
 
         return render(request, "propertyinfo/index.html", context)
