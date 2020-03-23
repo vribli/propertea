1e2fd076bd06f3d938af0169542a8cdd9da33e08
alice <Chua.alice1@gmail.com>
2020-03-24 01:10:43 +0800

    In progress :( Stupid SE i wanna do MH3511 BOOOOOO

--- search/views.py
+++ search/views.py
@@ -2,42 +2,50 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
+from pandas import *
+
 
 # Create your views here.
 def index(request):
     if True:
         keyword = request.GET['keyword']
-        #sortby = request.GET['sortby']
         filterby = request.GET['filterby']
+        district = request.GET['district']
 
+        if (keyword == 'nil') & (filterby == 'nil') & (district != 'nil'):
+            xls = ExcelFile("propertea/static/propertea.xlsx")
+            data = xls.parse(xls.sheet_names[0])
+            data = data.to_dict('index')
+            values = [v for v in data.values()]
+            resfiltered = [i for i in values if (i['DISTRICT'] == district)]
 
-        res = requests.get("https://developers.onemap.sg/commonapi/search?returnGeom=Y&getAddrDetails=Y&pageNum=1", params = {'searchVal' : keyword}).json()
+        else:
+            res = requests.get("https://developers.onemap.sg/commonapi/search?returnGeom=Y&getAddrDetails=Y&pageNum=1", params = {'searchVal' : keyword}).json()
 
-        resfiltered = [i for i in res.get('results') if not (i['POSTAL'] == 'NIL')]
+            resfiltered = [i for i in res.get('results') if not (i['POSTAL'] == 'NIL')]
 
-        for i in resfiltered:
-            if i['BUILDING'] == 'NIL':
-                i['BUILDING'] = i['ADDRESS']
-            # Python program to scrape website
-            # and save quotes from website
-            name = i['BUILDING'].replace(" ", "-")
-            URL = "https://www.squarefoot.com.sg/trends-and-analysis/residential?p={}".format(name)
-            URLL = "https://www.squarefoot.com.sg/trends-and-analysis/landed?p={}".format(name)
+            for i in resfiltered:
+                if i['BUILDING'] == 'NIL':
+                    i['BUILDING'] = i['ADDRESS']
+                # Python program to scrape website
+                # and save quotes from website
+                name = i['BUILDING'].replace(" ", "-")
+                URL = "https://www.squarefoot.com.sg/trends-and-analysis/residential?p={}".format(name)
+                URLL = "https://www.squarefoot.com.sg/trends-and-analysis/landed?p={}".format(name)
 
-            try:
-                r = requests.get(URL)
-                info_raw = BeautifulSoup(r.content).find('table', {"class": "minimalist", "width": "95%"}).text.replace("\n\n", "").replace("#", "").replace("*", "").split("\n")
-                ptype = "Non-Landed Residential"
-            except:
                 try:
-                    r = requests.get(URLL)
-                    info_raw = BeautifulSoup(r.content).find('table',{"class": "minimalist", "width": "95%"}).text.replace("\n\n", "").replace("#", "").replace("*", "").split("\n")
-                    ptype = "Landed Residential"
+                    r = requests.get(URL)
+                    info_raw = BeautifulSoup(r.content).find('table', {"class": "minimalist", "width": "95%"}).text.replace("\n\n", "").replace("#", "").replace("*", "").split("\n")
+                    ptype = "Non-Landed Residential"
                 except:
-                    ptype = None
+                    try:
+                        r = requests.get(URLL)
+                        info_raw = BeautifulSoup(r.content).find('table',{"class": "minimalist", "width": "95%"}).text.replace("\n\n", "").replace("#", "").replace("*", "").split("\n")
+                        ptype = "Landed Residential"
+                    except:
+                        ptype = None
 
-            i['TYPE'] = ptype
-
+                i['TYPE'] = ptype
 
         if filterby == "nonlanded":
             resfiltered2 = [i for i in resfiltered if (i['TYPE'] == "Non-Landed Residential")]
@@ -48,7 +56,8 @@ def index(request):
 
         context = {
             'keyword' : keyword,
-            'res' : resfiltered2
+            'res' : resfiltered2,
+            'district': district
         }
         return render(request, "search/index.html", context)
     #except KeyError:
