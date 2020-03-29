c83a588444ccc4926084efd4fa0174745fb4a506
vribli <vribli@gmail.com>
2020-03-29 21:06:07 +0800

    Merge Josephine's

--- propertyinfo/controller.py
+++ propertyinfo/controller.py
@@ -1,9 +1,4 @@
 import requests
-import numpy as np
-import pandas as pd
-import plotly.graph_objects as go
-from plotly.offline import plot
-from plotly.subplots import make_subplots
 from bs4 import BeautifulSoup
 from . import models
 
